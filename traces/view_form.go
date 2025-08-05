1c6d07151f9783b5a067d871735e34fa2bd07f23
marang <1550038+marang@users.noreply.github.com>
2025-08-05 16:10:25 +0200

    feat: confirm trace deletion and blur form on help focus

--- traces/view_form.go
+++ traces/view_form.go
@@ -7,7 +7,17 @@ import (
 // ViewForm renders the form for new traces.
 func (t *Component) ViewForm() string {
 	t.api.ResetElemPos()
+	focused := t.api.FocusedID() == IDForm
+	if t.form != nil {
+		if focused {
+			t.form.ApplyFocus()
+		} else {
+			for _, fld := range t.form.Fields {
+				fld.Blur()
+			}
+		}
+	}
 	content := t.form.View()
-	view := ui.LegendBox(content, "New Trace", t.api.Width()-2, 0, ui.ColBlue, true, -1)
+	view := ui.LegendBox(content, "New Trace", t.api.Width()-2, 0, ui.ColBlue, focused, -1)
 	return t.api.OverlayHelp(view)
 }
